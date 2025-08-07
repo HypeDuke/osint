0a457b2dc4b299ad5ec7ef49577d1d9724ec7677
highduck <huyduc1092000@gmail.com>
2025-08-07 13:57:46 +0700

    change var

--- backend/models.py
+++ backend/models.py
@@ -17,7 +17,7 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    password_hash = Column(String)
 
 def get_db():
     db = SessionLocal()
